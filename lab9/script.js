681a20e7ecbcb123727db8f1811372a2d34ebdaa
Hieu <tranngochieu98.tb@gmail.com>
2021-03-30 11:24:45 -0300

    baitap

---
+++ lab9/script.js
@@ -0,0 +1,37 @@
+// let a = prompt("nhap so");
+// function mm(a) {
+//   return a / 10;
+// }
+// function m(a) {
+//   return a * 100;
+// }
+
+// function km(a) {
+//   return a * 1000;
+// }
+
+// console.log("mm: " + mm(a));
+// console.log("m: "+m(a));
+// console.log("km: " + km(a));
+
+// let a = prompt("Celsius");
+
+// function Fahrenheit(a) {
+//     return (a * 9/5) + 32;
+// }
+// function Kevin(a) {
+//     return a + 273.15;
+// }
+// console.log("Fahrenheit : " + Fahrenheit(a));
+// console.log("Kevin : " + Kevin(a));
+
+let a = prompt("time-s")
+function phut(a) {
+   return a / 60;
+}
+
+function gio(a) {
+    return a / 3600;
+}
+console.log("phut : " + phut(a));
+console.log("gio : " + gio(a));
